1944917e22aff99633ec59244e877827d8c730ff
brunedv <vianney.bruned@gmail.com>
2020-10-17 20:50:03 +0200

    remove initial bug

--- app.py
+++ app.py
@@ -99,34 +99,36 @@ def generate_param_box(distribution):
     [State('Distribution-Law', 'value'), State({'type': 'param', 'index': ALL}, 'value')]
 )
 def generate_graph(n_clicks, n_simulation, sample_size, distribution, values):
+    if len(values)!=0:
+        if distribution=='uniform':
+            x_tot = np.random.uniform(low= values[0], high= values[1], size=(n_simulation, sample_size))
+            mu = np.mean(values)
+            sigma = np.sqrt((values[1]-values[0])**2/12 / sample_size)
+        elif distribution=='normal':
+            x_tot = np.random.normal(loc= values[0], scale= values[1], size=(n_simulation, sample_size))
+            mu = values[0]
+            sigma = values[1] / np.sqrt(sample_size)
+        elif distribution=='beta':
+            x_tot = np.random.beta(a= values[0], b= values[1], size=(n_simulation, sample_size))
+            mu = values[0]/np.sum(values)
+            a = values[0]
+            b = values[1] 
+            sigma =  np.sqrt(((a*b)/((a+b)**2*(a+b+1))) /sample_size)
 
-    if distribution=='uniform':
-        x_tot = np.random.uniform(low= values[0], high= values[1], size=(n_simulation, sample_size))
-        mu = np.mean(values)
-        sigma = np.sqrt((values[1]-values[0])**2/12 / sample_size)
-    elif distribution=='normal':
-        x_tot = np.random.normal(loc= values[0], scale= values[1], size=(n_simulation, sample_size))
-        mu = values[0]
-        sigma = values[1] / np.sqrt(sample_size)
-    elif distribution=='beta':
-        x_tot = np.random.beta(a= values[0], b= values[1], size=(n_simulation, sample_size))
-        mu = values[0]/np.sum(values)
-        a = values[0]
-        b = values[1] 
-        sigma =  np.sqrt(((a*b)/((a+b)**2*(a+b+1))) /sample_size)
 
-
-    
-    x = np.mean(x_tot, axis=1)
-    max_x, min_x = np.max(x), np.min(x)
-    bin_size =  (max_x-min_x)*10/len(x) #3.49*np.std(x)*np.power(sample_size,-1/3)
-    fig = ff.create_distplot([x], bin_size=[bin_size], group_labels = [distribution], curve_type='normal')
-    print(x.shape)
-    dist = norm(mu, sigma)
-    x_pdf = np.linspace(dist.ppf(0.01),
-                dist.ppf(0.99), 100)
-    fig.add_trace(go.Scatter(x=x_pdf, y=dist.pdf(x_pdf),name='CLT'))   
-    return fig
+        
+        x = np.mean(x_tot, axis=1)
+        max_x, min_x = np.max(x), np.min(x)
+        bin_size =  (max_x-min_x)*10/len(x) #3.49*np.std(x)*np.power(sample_size,-1/3)
+        fig = ff.create_distplot([x], bin_size=[bin_size], group_labels = [distribution], curve_type='normal')
+        print(x.shape)
+        dist = norm(mu, sigma)
+        x_pdf = np.linspace(dist.ppf(0.01),
+                    dist.ppf(0.99), 100)
+        fig.add_trace(go.Scatter(x=x_pdf, y=dist.pdf(x_pdf),name='CLT'))   
+        return fig
+    else:
+        return []
 
 if __name__ == '__main__':
     server.run(host='0.0.0.0')      
